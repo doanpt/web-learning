ff74678a289f14a4a87937482729c7193fb63c06
Trung Doan <doanpt93cntt@gmail.com>
2021-09-19 16:51:31 +0700

    create card list components to display robots from list

--- robo-friends-react/src/Card.js
+++ robo-friends-react/src/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 const Card = ({id, name, email}) => {
     return(
         <div className="bg-light-green dib br3 pa3 ma2 grow shadow-5">
-            <img src={`https://robohash.org/${id}?200x200' alt='robots'`}/>
+            <img src={`https://robohash.org/${id}?200x200'`} alt='robots'/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
